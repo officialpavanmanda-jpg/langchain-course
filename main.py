db28fd5cd97b5df792297c9aa83bf0c021887c89
pawanManda <pavan.manda.mp@gmail.com>
2026-04-22 00:03:30 +0530

    search agent

--- main.py
+++ main.py
@@ -3,12 +3,35 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 
+@tool
+def search(query: str) -> str:
+    """
+    Tools that searches over internet
+    Args: 
+        query: The query to search for
+    Returns:
+        The search result
+    """
+    print(f"Searching for {query}")
+    return "Tokyo's weather is sunny"
+
+ollama_model = os.getenv("OLLAMA_MODEL", "llama3.1:8b")
+llm = ChatOllama(ollama_model)
+tools = [search]
+agent = create_agent(model=llm,tools=tools)
 
 def main():
-    print("Hello from langchain-course!")
-    print(os.getenv("GOOGLE_API_KEY"))
-
+    print("Hello from langchain-courses search-agent")
+    result = agent.invoke(
+        {"messages": [HumanMessage(content="What is the weather in Tokyo ?")]}
+    )
+    print(result)
 
 if __name__ == "__main__":
     main()
