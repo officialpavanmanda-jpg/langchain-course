7f366a183a82be2c6092b1a5bbd9fa1453c420ba
pawanManda <pavan.manda.mp@gmail.com>
2026-05-09 18:45:27 +0530

    document ingestion pipeline

--- ingestion.py
+++ ingestion.py
@@ -9,5 +9,20 @@ from langchain_pinecone import PineconeVectorStore
 load_dotenv()
 
 if __name__ == '__main__':
-    print("Ingesting...")
  #  print(os.environ['PINECONE_API_KEY'])
+
+    print("Ingesting...")
+    loader = TextLoader("C:/Users/pavan/Desktop/langchain-course/mediumblog1.txt", encoding="utf-8")
+    document = loader.load()
+
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+    
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+    print("ingesting ....2....")
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ.get("INDEX_NAME"))
+    print("ingestion done!")
